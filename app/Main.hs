9e602c419e9ba08303f62c81e458567dc47acb93
Justyna <Piotr_Justyna@EMEA.DELL.COM>
2018-04-03 13:03:54 +0100

    PUT request.

--- app/Main.hs
+++ app/Main.hs
@@ -9,7 +9,9 @@ module Main where
 import Lib
 import Models
 
-import qualified Data.Text as T
+import qualified  Data.ByteString.Lazy as BS
+import qualified  Data.Text as T
+import Data.Aeson (encode)
 import Yesod
 
 data App =
@@ -20,20 +22,24 @@ mkYesod
   [parseRoutes|
 /                       HomeR           GET
 /feedback               FeedbackR       GET
-/feedback/#T.Text       FeedbackByIdR   GET
+/feedback/#T.Text       FeedbackByIdR   GET PUT
 |]
 
 instance Yesod App
 
 getFeedbackR :: Handler Value
-getFeedbackR =
-  returnJson
-    [ Feedback "1" "poor" "it wasn't great"
-    , Feedback "2" "pretty bad" "nope."
-    ]
+getFeedbackR = returnJson [Feedback "1" "poor" "it wasn't great", Feedback "2" "pretty bad" "nope."]
 
 getFeedbackByIdR :: T.Text -> Handler Value
 getFeedbackByIdR x = returnJson $ Feedback x "ok" "passable"
+
+putFeedbackByIdR :: T.Text -> Handler Value
+putFeedbackByIdR x = do
+  liftIO $ BS.writeFile path (encode newFeedback)
+  returnJson newFeedback
+  where
+    path = "feedback/" ++ (T.unpack x) ++ ".json"
+    newFeedback = Feedback x "excellent" "result of HTTP PUT"
 
 getHomeR :: Handler String
 getHomeR = return "Welcome to Feedback on Anything."
